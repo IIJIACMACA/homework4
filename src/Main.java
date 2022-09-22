0ef4f3ecc8798c5b5efe48c245ef27ddcae8b7d1
IIJIACMACA <dimasik696969@mail.ru>
2022-09-22 20:52:39 +0300

    дз 4 готово

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+     var qWight = 640;
+     var workpiplehourWight =8;
+     var pipleWigth = qWight / workpiplehourWight;
+        System.out.println("всего работников в компании- "+pipleWigth+ " человек");
+        pipleWigth = pipleWigth + 94;
+        System.out.println("работает " + pipleWigth);
+        qWight = pipleWigth * workpiplehourWight;
+        System.out.println("если в компании работает - " + pipleWigth+ " человек, то всего " +qWight+ " часов работы может быть поделено между сотрудниками");
+
+
 
     }
 }
